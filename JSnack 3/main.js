6b8853e5a15dcdcdaac195d86f0466e74a597239
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-03 16:57:54 +0200

    [UPD]

--- JSnack 3/main.js
+++ JSnack 3/main.js
@@ -3,7 +3,7 @@
 let addition = 0;
 
 for (let index = 0; index < 10; index++) {
-  const userNumber = parseInt(prompt("Inserisci un numero"));
+  const userNumber = parseInt(prompt(`${index} di 10 - Inserisci un numero`));
   console.log(userNumber);
   addition += userNumber;
 }
