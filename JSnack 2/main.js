9c167158d4e5fad935523bf8cba2076dbdc8121a
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-03 11:55:35 +0200

    [UPD] Exercise done

--- JSnack 2/main.js
+++ JSnack 2/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const firstWord = prompt("Inserisci la prima parola");
-const secondWord = prompt("Inserisci la seconda parola");
+const firstWord = prompt("Inserisci la prima parola").length;
+const secondWord = prompt("Inserisci la seconda parola").length;
 
 console.log(firstWord);
 console.log(secondWord);
@@ -9,7 +9,7 @@ console.log(secondWord);
 if (firstWord < secondWord) {
   console.log("La seconda parola è più lunga");
 } else if (firstWord > secondWord) {
-  console.log("La seconda parola è più lunga");
+  console.log("La prima parola è più lunga");
 } else {
   console.log("Le due parole sono lunghe uguali");
 }
