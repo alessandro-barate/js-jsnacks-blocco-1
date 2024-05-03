f10be587db7c4dc109b15812cf1d6acd9646cb72
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-03 12:43:58 +0200

    [UPD] JSnack 3 done

--- JSnack 3/main.js
+++ JSnack 3/main.js
@@ -1,6 +1,11 @@
 "use strict";
 
+let addition = 0;
+
 for (let index = 0; index < 10; index++) {
-  const userNumber = prompt("Inserisci un numero");
+  const userNumber = parseInt(prompt("Inserisci un numero"));
   console.log(userNumber);
+  addition += userNumber;
 }
+
+console.log(addition);
